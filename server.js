749665a1e919e22005a6bd2108d6c4bd9bc621e8
nycki <nupanick@gmail.com>
2026-02-18 12:57:39 -0800

    ejs and markdown together?

--- server.js
+++ server.js
@@ -3,12 +3,24 @@
 import express from 'express';
 import sqlite3 from 'sqlite3';
 import ejs from 'ejs';
+import fs from 'node:fs/promises';
+import rehypeStringify from 'rehype-stringify';
+import remarkParse from 'remark-parse';
+import remarkRehype from 'remark-rehype';
+import { unified } from 'unified';
 
 const app = express();
+app.use(express.static('site/static'));
+
 const db = new sqlite3.Database('sqlite.db', (err) => {
   if (err) return console.log(err.message);
   console.log('connected to database.');
 });
+
+const md = unified();
+md.use(remarkParse);
+md.use(remarkRehype, { allowDangerousHtml: true });
+md.use(rehypeStringify, { allowDangerousCharacters: true });
 
 db.run(`
   CREATE TABLE IF NOT EXISTS pages (
@@ -41,7 +53,13 @@ app.get('/', (req, res) => {
   });
 });
 
-app.use(express.static('site/static'));
+app.get('/about', async (req, res) => {
+  let body = await fs.readFile('site/template/about.ejs.md');
+  body = ejs.render(String(body));
+  body = await md.process(body);
+  body = String(body);
+  res.send(body);
+})
 
 app.listen(8080);
 console.log('server on http://localhost:8080');
---
+++ site/template/about.ejs.md
@@ -0,0 +1,3 @@
+# Hello from Markdown
+
+<%= "Hello from EJS" %>
